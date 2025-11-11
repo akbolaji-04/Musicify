a0f99d911ecd64b8c1b07e0ef19159d686b023b1
akbolaji-04 <akbolaji04@gmail.com>
2025-11-11 20:06:49 +0100

    fixed vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
 });
 
